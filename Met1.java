749aabcfc690f678c7cf1c12d509c269cd38673d
Enzo D. Prado <84857077+EnzoDPrado@users.noreply.github.com>
2022-04-20 09:59:46 -0300

    Add files via upload

--- Met1.java
+++ Met1.java
@@ -3,46 +3,66 @@ class Met1{
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
 
-        double sim = input.nextDouble();
-        double nao = input.nextDouble();
 
-        double teste1 = somar(sim, nao);
-        System.out.println("somar= " + teste1);
+        double[] info = entrada();
+        double valor1 = info[0];
+        double valor2 = info[1];
 
-        double teste2 = subtrair(sim, nao);
-        System.out.println("Subtrair= " + teste2);
+        double soma = somar(valor1, valor2);
+        double diferenca = subtrair(valor1, valor2);
+        double produto= multiplicar(valor1, valor2);
+        double quociente = divisao(valor1, valor2);
 
-        double teste3= multiplicar(sim, nao);
-        System.out.println("Multiplicar= " + teste3);
-
-        double teste4 = divisao(sim, nao);
-        System.out.println("divisão= " + teste4);
-
+        saida(valor1, '+', valor2, soma);
+        
 
 
     }
 
+
+    //método para somar
     public static double somar(double n1, double n2){
         return(n1+n2);
     }
 
+    //método para subtrair
     public static double subtrair(double n1, double n2){
         return(n1-n2);
     }
 
+    //método para multiplicar 
     public static double multiplicar(double n1, double n2){
         return(n1*n2);
     }
 
+    //método para dividir
     public static double divisao(double n1, double n2){
         if(n2 == 0){
-            System.out.println("É impossível dividir por 0");
-            System.exit(0);
+            return -1;
         }
          return(n1/n2);
-        
+    }
+
+    //método para entrada de dados
+    public static double[] entrada(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro valor");
+        double valor1 = input.nextDouble();
+        System.out.println("Digite o segundo valor");
+        double valor2 = input.nextDouble();
+
+        double[] valores = {valor1, valor2};
+        return valores;
+    }
+
+    //método para exibir a saida
+    public static void saida(double val1,char op, double val2, double result){
+        System.out.println(val1 + " " + op + val2 + " = " + res);
+
         
     }
+    
 
 } 
 
